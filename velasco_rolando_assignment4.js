cfc0232b2cec9bc3b312275a7142ff46d453e4a8
Rolando Velasco <rvelasco@fullsail.edu>
2013-01-28 23:27:37 -0800

    updated html file, wrote function isStringAPhoneNumber.

--- velasco_rolando_assignment4.js
+++ velasco_rolando_assignment4.js
@@ -1,1 +1,47 @@
-alert("JavaScript works!");
+// Rolando Velasco
+// 01-31-2013
+// Project 4
+// Code library
+
+// Check if the string passed as the argument is a phone number (follows pattern XXX-XXX-XXXX)
+// Return true if it is a phone number or false if it is not.
+var isStringAPhoneNumber = function(string) {
+	
+	// First, check that the dashes are in the correct locations of the string
+	if ((string.indexOf("-") === 3) && (string.lastIndexOf("-") === 7)) {
+	
+		// Second, check that the length of the string is the same length as a phone number
+		if (string.length === 12) {
+			return true;
+		} else {
+			return false;
+		};
+	} else {
+		return false;
+	};
+};
+
+var isStringAnEmail = function() {
+
+
+};
+
+var isStringAURL = function() {
+
+};
+
+var titleCaseString = function() {
+
+};
+
+var changeSeparator = function() {
+
+};
+
+
+// main code to test library functions
+var string = "123-456-7890";
+var isPhoneNumber = isStringAPhoneNumber(string);
+console.log("Calling function isStringAPhoneNumber(" + string + ").");
+console.log("Is this a phone number? " + isPhoneNumber);
+ 
